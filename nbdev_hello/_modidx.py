0b731d12a4921b07e712068164c25f2315b0d499
Hiroshi Doyu <hiroshi.doyu@gmail.com>
2023-02-16 18:45:12 +0200

    update

--- nbdev_hello/_modidx.py
+++ nbdev_hello/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://doyu.github.io',
                 'git_url': 'https://github.com/doyu/nbdev-hello',
                 'lib_path': 'nbdev_hello'},
-  'syms': {'nbdev_hello.core': {'nbdev_hello.core.foo': ('core.html#foo', 'nbdev_hello/core.py')}}}
+  'syms': { 'nbdev_hello.core': { 'nbdev_hello.core.bar': ('core.html#bar', 'nbdev_hello/core.py'),
+                                  'nbdev_hello.core.foo': ('core.html#foo', 'nbdev_hello/core.py')}}}
